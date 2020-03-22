befbf5be690b05c548c610feedcb5419cae4fad8
Jaime Blasco <git@jaimeblasco.com>
2020-03-22 12:24:49 +0100

    Add example project using intl_translation_arb

---
+++ intl_translation_example/android/app/src/main/kotlin/intltranslationexample/intl_translation_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package intltranslationexample.intl_translation_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ intl_translation_example/extract.sh
@@ -0,0 +1,1 @@
+ flutter pub run intl_translation_arb:extract_to_arb lib/main.dart --locale en --output-dir lib/l10n/ --output-file intl_messages_en.arb
