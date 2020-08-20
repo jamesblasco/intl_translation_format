b10ec29ec80ef4c491ee1dc63a19fef23109cfa7
Jaime Blasco <git@jaimeblasco.com>
2020-08-20 11:14:43 +0200

    Update travis

--- tool/travis.sh
+++ tool/travis.sh
@@ -55,8 +55,8 @@ for PKG in ${PKGS}; do
     echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
     case ${TASK} in
     command)
-      echo 'pub run build_runner test --delete-conflicting-outputs -- -P presubmit'
-      pub run build_runner test --delete-conflicting-outputs -- -P presubmit || EXIT_CODE=$?
+      echo 'pub run test'
+      pub run test || EXIT_CODE=$?
       ;;
     dartanalyzer)
       echo 'dartanalyzer --fatal-infos --fatal-warnings .'
