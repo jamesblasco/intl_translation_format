f0a078ab2aa688198f09bcdf749e08eda980943f
Jaime Blasco <git@jaimeblasco.com>
2020-08-28 19:48:14 +0200

    License + Upload pre-release packages to pub.dev

--- intl_translation_format/lib/src/utils/formats.dart
+++ intl_translation_format/lib/src/utils/formats.dart
@@ -1,8 +1,4 @@
-import 'package:intl_translation_arb/intl_translation_arb.dart';
 import 'package:intl_translation_format/intl_translation_format.dart';
-import 'package:intl_translation_xliff/intl_translation_xliff.dart';
-
-import '../translation_format.dart';
 
 export 'package:intl_translation_arb/intl_translation_arb.dart';
 export 'package:intl_translation_xliff/intl_translation_xliff.dart';
